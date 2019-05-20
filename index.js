a5540abf052f8c9415cda1c5c573b5b7aacb5e39
Eunhee Jo <joeun2ce@gmail.com>
2019-05-20 16:35:27 +0900

    Add Start block dirty flags

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const { web3, ethContract } = require("./handlerVersions/v1/web3");
 
 const editJsonFile = require("edit-json-file");
 const file = editJsonFile("./.gxc-data.json");
+const nodeFlags = require("node-flag");
 
+if(nodeFlags.isset("dirty")) {
+
+}
 if( file.get("node.dirty") === true ) {
    console.warn("=== node is dirty ! ===\n dirty is", file.get("node.dirty"));
    process.exit(1);
@@ -17,7 +21,7 @@ if( file.get("node.dirty") === true ) {
       const actionHandler = new ObjectActionHandler([handlerVersion]);
 
       const actionReader = new NodeosActionReader({
-         startAtBlock: file.get("node.startAtBlock") === null ? 0 : file.get("node.startAtBlock"),
+         startAtBlock: nodeFlags.isset("dirty")? 0 : ( file.get("node.startAtBlock") === null ? 0 : file.get("node.startAtBlock") ),
          onlyIrreversible: false,
          nodeosEndpoint: file.get("node.nodeosEndpoint") === null ? "127.0.0.1:9999" : file.get("node.nodeosEndpoint"),
       });
