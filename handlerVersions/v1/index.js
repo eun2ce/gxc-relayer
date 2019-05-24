e8a6c1d04db3bba9985e7e4f93bbec76bb4fd48e
Eunhee Jo <joeun2ce@gmail.com>
2019-05-24 18:11:48 +0900

    Updater newcontract async funtion

--- handlerVersions/v1/index.js
+++ handlerVersions/v1/index.js
@@ -13,32 +13,37 @@ function parseAccount( act ) {
    return headAct.concat(act);
 }
 
+const newcontract = async (payload) => {
+   console.info("\n newContract \n");
+   const floatValue = parseFloatEth(payload.data.value);
+   const recipient = parseAccount(payload.data.recipient[1]);
+   const timelock = Math.floor(Date.parse(((payload.data.timelock) + "Z")) / 1000);
+
+   try {
+      const result = await ethContract.methods.newContract(
+         file.get("contract.vaultAddress"),
+         recipient,
+         file.get("contract.tokenContractId"),
+         web3.utils.toWei(floatValue, "ether"),
+         parseAccount(payload.data.hashlock),
+         timelock
+      ).send({ from: file.get("contract.vaultAddress"), gas: "0x47E7C4" });
+   } catch(e) {
+      console.info({e}); }
+}
+
 function updateNewcontractData(state, payload, blockInfo, context) {
    if(payload.data.recipient[0] !== "checksum160"){
       console.info(payload.data);
       return;
    }
-   console.log("\n newContract \n");
 
-   const floatValue = parseFloatEth(payload.data.value);
-   const recipient = parseAccount(payload.data.recipient[1]);
-   const timelock = Math.floor(Date.parse(((payload.data.timelock) + "Z")) / 1000);
-
-   (async()=> {
-      console.log("\n newContract in async \n");
+   (async() => {
       const getContract = await ethContract.methods.getContract(payload.data.contract_name).call({ from: file.get("contract.vaultAddress") });
       if(getContract.sender === "0x0000000000000000000000000000000000000000"){
-         const newContract = await ethContract.methods.newContract(
-            file.get("contract.vaultAddress"),
-            recipient,
-            file.get("contract.tokenContractId"),
-            web3.utils.toWei(floatValue, "ether"),
-            parseAccount(payload.data.hashlock),
-            timelock
-         ).send({ from: file.get("contract.vaultAddress"), gas: "0x47E7C4" });
-         console.info({newContract});
+         (async () => await newcontract(payload))();
       } else { console.info({ getContract }) };
-   })()
+   })();
 }
 
 const updaters = [
